9d2932a160a625f44f3052496025598575fccc6d
Srinidhi-06-cmd <srinidhik74@gmail.com>
2026-03-11 18:45:48 +0530

    Add test for subtraction in AppTest class

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,10 +1,23 @@
 package com.example;
+
 import org.junit.Test;
 import static org.junit.Assert.*;
-public class AppTest{
-@Test
-public void testAdd(){
-  App app = new App();
-  assertEquals(5,app.add(2,3));
-}
+
+public class AppTest {
+
+    @Test
+    public void testAdd() {
+
+        App app = new App();
+
+        assertEquals(5, app.add(2,3));
+    }
+
+    @Test
+    public void testSubtract() {
+
+        App app = new App();
+
+        assertEquals(2, app.subtract(5,3));
+    }
 }
